d066261aee1eaac4bfe313f63f5ba00d29389422
Agustin Bettati <bettatiagustin@gmail.com>
2017-09-28 23:43:05 -0300

    Plot de un grafico para probar librerias matplotlib y csv

---
+++ tp2/heightAltitudGraph.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import csv
+
+
+file = open("resource/geo_pos.csv", 'r')
+
+reader = csv.reader(file)
+
+next(reader)
+
+for row in reader:
+    plt.plot(row[0], row[2], 'bo')
+
+plt.xlabel('Altura')
+plt.ylabel('Longitud')
+plt.title('Longitud en funcion de la altura')
+plt.axis([10100, 0, -3, 41])
+plt.show()
