20c7bc78bb79683df4171d3889c044aad73fe174
Agustin Bettati <bettatiagustin@gmail.com>
2017-11-18 12:16:50 -0300

    Added ambientTemp to methods

--- tp3/newtonCoolingLaw.py
+++ tp3/newtonCoolingLaw.py
@@ -5,14 +5,15 @@ def analyticSolution(time, initTemp, ambientTemp):
     return ambientTemp + math.exp(-0.25*time)*(initTemp - ambientTemp)
 
 
-def rk4(f, x0, y0, xf, n):
+# Toma la una f(tiempo, temp, ambientTemp) .. x0 y y0 son el tiempo y
+# temperatura previa, xf es el instante t a calcular, y ambientTemp
+def rk4(f, x0, y0, ambientTemp, xf, n):
     vx = [0] * (n + 1)
     vy = [0] * (n + 1)
     h = (xf - x0) / float(n)
     vx[0] = x = x0
     vy[0] = y = y0
     for i in range(1, n + 1):
-        ambientTemp = 50
         k1 = h * f(x, y, ambientTemp)
         k2 = h * f(x + 0.5 * h, y + 0.5 * k1, ambientTemp)
         k3 = h * f(x + 0.5 * h, y + 0.5 * k2, ambientTemp)
@@ -27,9 +28,9 @@ def f(time, temp, ambientTemp):
 
 
 
-vx, vy = rk4(f, 0, 80, 10, 100)
+vx, vy = rk4(f, 0, 80, 50, 10, 10)
 for x, y in list(zip(vx, vy)):
-    print("%4.1f %10.5f %+12.4e" % (x, y, y - (4 + x * x) ** 2 / 16))
+    print("%4.1f %10.5f %10.5f" % (x, y, analyticSolution(x,80,50)))
 
 print(analyticSolution(10, 80, 50))
 
