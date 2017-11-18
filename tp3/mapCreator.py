55b179d479a5f41393e989227be958172df777ec
Agustin Bettati <bettatiagustin@gmail.com>
2017-11-18 14:42:50 -0300

    csv generate using analytic and rungeKutta solution

---
+++ tp3/mapCreator.py
@@ -0,0 +1,66 @@
+import csv
+from tp3.newtonCoolingLaw import rk4
+from tp3.newtonCoolingLaw import analyticSolution
+
+file = open("rungeKutta/t0.csv", 'r')  # read only
+
+reader = csv.reader(file)
+
+t0values = []
+
+for row in reader:
+    aux = []
+    for i in range(0,22):
+        aux.append(int(row[i]))
+    t0values.append(aux)
+
+prevTemp = t0values
+
+
+def ambientTemp(i, j, prevTemp):
+    sum = 0.0
+    for x in range(i-1, i+2):
+        if(x == i):
+            sum += prevTemp[x][j-1]
+            sum += prevTemp[x][j+1]
+        else:
+            for y in range(j-1, j+2):
+                sum += prevTemp[x][y]
+
+    return sum / 8.0
+
+def createCsv(temps, time):
+    file = open("rungeKutta/t"+ str(time)+".csv", "w")
+    for i in range(0, 22):
+        for j in range(0, 21):
+            file.write("{0:.2f}".format(temps[i][j]) + ", ")
+        file.write("{0:.2f}".format(temps[i][21]) + "\n")
+
+for time in range(1, 11):
+    newTemp = []
+
+    wall = []
+    for i in range(0, 22):
+        wall.append(15)
+
+    newTemp.append(wall)
+    for i in range(1,21):
+        aux = []
+        aux.append(15)
+        for j in range(1,21):
+            ambTemp = ambientTemp(i,j, prevTemp)
+            # temp = analyticSolution(10, prevTemp[i][j], ambTemp)
+            temp = rk4(0, prevTemp[i][j],ambTemp,10,1)
+            aux.append(temp)
+        aux.append(15)
+        newTemp.append(aux)
+
+    newTemp.append(wall)
+    createCsv(newTemp, time)
+    prevTemp = newTemp
+
+
+
+
+
+
--- tp3/newtonCoolingLaw.py
+++ tp3/newtonCoolingLaw.py
@@ -7,30 +7,36 @@ def analyticSolution(time, initTemp, ambientTemp):
 
 # Toma la una f(tiempo, temp, ambientTemp) .. x0 y y0 son el tiempo y
 # temperatura previa, xf es el instante t a calcular, y ambientTemp
-def rk4(f, x0, y0, ambientTemp, xf, n):
+def rk4(x0, y0, ambientTemp, xf, n):
+
+    def f(time, temp):
+        return -0.25 * (temp - ambientTemp)
+
     vx = [0] * (n + 1)
     vy = [0] * (n + 1)
     h = (xf - x0) / float(n)
     vx[0] = x = x0
     vy[0] = y = y0
     for i in range(1, n + 1):
-        k1 = h * f(x, y, ambientTemp)
-        k2 = h * f(x + 0.5 * h, y + 0.5 * k1, ambientTemp)
-        k3 = h * f(x + 0.5 * h, y + 0.5 * k2, ambientTemp)
-        k4 = h * f(x + h, y + k3, ambientTemp)
+        k1 = h * f(x, y)
+        k2 = h * f(x + 0.5 * h, y + 0.5 * k1)
+        k3 = h * f(x + 0.5 * h, y + 0.5 * k2)
+        k4 = h * f(x + h, y + k3)
         vx[i] = x = x0 + i * h
         vy[i] = y = y + (k1 + k2 + k2 + k3 + k3 + k4) / 6
-    return vx, vy
-
-
-def f(time, temp, ambientTemp):
-    return -0.25*(temp - ambientTemp)
+    return vy[n]
 
 
 
-vx, vy = rk4(f, 0, 80, 50, 10, 10)
-for x, y in list(zip(vx, vy)):
-    print("%4.1f %10.5f %10.5f" % (x, y, analyticSolution(x,80,50)))
 
-print(analyticSolution(10, 80, 50))
+
+# t1 = rk4(0, 80, 50, 10, 10)
+# print(t1)
+# anat1 = analyticSolution(10, 80, 50)
+# print(anat1)
+#
+# t2 = rk4(0, t1, 60, 10, 10)
+# print(t2)
+# anat2 = analyticSolution(10, anat1, 60)
+# print(anat2)
 
